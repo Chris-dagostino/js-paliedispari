28205f335e8e0026d4520e5ad1ecd28658ab59eb
Chris-dagostino <dagostino.christian94@gmail.com>
2022-01-28 15:54:24 +0100

    pari o dispari

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,9 +1,15 @@
 //!L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
 
+
+let scelta = prompt ('scegli pari o dispari');
+
+
 let NumeroUtente = parseInt(prompt("inserisci un numero da 1 a 5"));
 
 
 console.log(NumeroUtente);
+console.log(scelta);
+
 
 
 //!Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
